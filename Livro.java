7299064d8e422f5f0264a04ff077ecdcc25ed4db
thaisbraghini <thaisbraghinisilva@gmail.com>
2025-06-19 19:37:37 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -1,9 +1,7 @@
-package Arvores.Projeto4;
-
-public class Livro {
-    String titulo;
-    String autor;
-    int ano;
+public class Livro implements Comparable<Livro> {
+    private String titulo;
+    private String autor;
+    private int ano;
 
     public Livro(String titulo, String autor, int ano) {
         this.titulo = titulo;
@@ -11,8 +9,32 @@ public class Livro {
         this.ano = ano;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
     @Override
     public String toString() {
-        return titulo + " (" + ano + ")";
+        return "Livro [getTitulo()=" + getTitulo() + ", getAutor()=" + getAutor() + ", getAno()=" + getAno() + "]";
+    }
+
+    private String geraChave(){
+        String chave = (this.getAutor() + this.getTitulo() + this.getAno()).toLowerCase();
+        return chave;
+    }
+
+    @Override
+    public int compareTo(Livro outroLivro) {
+        String chaveThis = this.geraChave();
+        String chaveOutro = outroLivro.geraChave();
+        return chaveThis.compareTo(chaveOutro);
     }
 }
